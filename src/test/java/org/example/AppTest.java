029fd185c8ccec2b5fd85408bdee1da1ecc22e1f
Lucian B <lucian.bratu@stud.ubbcluj.ro>
2024-03-20 09:32:25 +0200

    Implement test cases

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,5 +1,42 @@
 package org.example;
 
-public class AppTest {
+import org.example.domain.Student;
+import org.example.repository.NotaXMLRepo;
+import org.example.repository.StudentXMLRepo;
+import org.example.repository.TemaXMLRepo;
+import org.example.service.Service;
+import org.example.validation.NotaValidator;
+import org.example.validation.StudentValidator;
+import org.example.validation.TemaValidator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+public class AppTest {
+    Service service;
+    Student student;
+    @BeforeEach
+    void setUp() {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        this.service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+    }
+
+    @Test
+    void addStudentSuccessful() {
+        this.student = new Student("300", "nfaib", 100, "ewofohewf");
+        this.service.addStudent(this.student);
+    }
+
+    @Test
+    void addStudentFailure() {
+        this.student = new Student("", "nfaib", 100, "ewofohewf");
+        this.service.addStudent(this.student);
+    }
 }
