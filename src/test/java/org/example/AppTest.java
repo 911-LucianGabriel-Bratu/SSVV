e26b0125137be466ece75ee26e41c1582cc0f006
Lucian B <lucian.bratu@stud.ubbcluj.ro>
2024-04-15 21:30:47 +0300

    Add assignment test cases

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -9,8 +9,11 @@ import org.example.service.Service;
 import org.example.validation.NotaValidator;
 import org.example.validation.StudentValidator;
 import org.example.validation.TemaValidator;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class AppTest {
     Service service;
@@ -30,84 +33,84 @@ public class AppTest {
         this.service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
     }
 
-    @Test
-    void addStudentSuccessful() {
-        this.student = new Student("300", "nfaib", 100, "ewofohewf");
-        this.service.deleteStudent(this.student.getID());
-        assert this.service.addStudent(this.student) == null;
-    }
-
-    @Test
-    void addStudentIdEmptyFailure() {
-        this.student = new Student("", "nfaib", 100, "ewofohewf");
-        try {
-            this.service.addStudent(this.student);
-            assert false;
-        }
-        catch (Exception e) {
-
-        }
-    }
-
-    @Test
-    void addStudentIdNullFailure() {
-        this.student = new Student(null, "nfaib", 100, "ewofohewf");
-        try {
-            this.service.addStudent(this.student);
-            assert false;
-        }
-        catch (Exception e) {}
-    }
-
-    @Test
-    void addStudentNumeEmptyFailure() {
-        this.student = new Student("300", "", 100, "ewofohewf");
-        try {
-            this.service.addStudent(this.student);
-            assert false;
-        }
-        catch (Exception e) {}
-    }
-
-    @Test
-    void addStudentNumeNullFailure() {
-        this.student = new Student("300", null, 100, "ewofohewf");
-        try {
-            this.service.addStudent(this.student);
-            assert false;
-        }
-        catch (Exception e) {}
-    }
-
-    @Test
-    void addStudentGrupaLessThan0Failure() {
-        this.student = new Student("300", "nfaib", -1, "ewofohewf");
-        try {
-            this.service.addStudent(this.student);
-            assert false;
-        }
-        catch (Exception e) {}
-    }
-
-    @Test
-    void addStudentEmailEmptyFailure() {
-        this.student = new Student("300", "nfaib", 100, "");
-        try {
-            this.service.addStudent(this.student);
-            assert false;
-        }
-        catch (Exception e) {}
-    }
-
-    @Test
-    void addStudentEmailNullFailure() {
-        this.student = new Student("300", "nfaib", 100, null);
-        try {
-            this.service.addStudent(this.student);
-            assert false;
-        }
-        catch (Exception e) {}
-    }
+//    @Test
+//    void addStudentSuccessful() {
+//        this.student = new Student("300", "nfaib", 100, "ewofohewf");
+//        this.service.deleteStudent(this.student.getID());
+//        assert this.service.addStudent(this.student) == null;
+//    }
+//
+//    @Test
+//    void addStudentIdEmptyFailure() {
+//        this.student = new Student("", "nfaib", 100, "ewofohewf");
+//        try {
+//            this.service.addStudent(this.student);
+//            assert false;
+//        }
+//        catch (Exception e) {
+//
+//        }
+//    }
+//
+//    @Test
+//    void addStudentIdNullFailure() {
+//        this.student = new Student(null, "nfaib", 100, "ewofohewf");
+//        try {
+//            this.service.addStudent(this.student);
+//            assert false;
+//        }
+//        catch (Exception e) {}
+//    }
+//
+//    @Test
+//    void addStudentNumeEmptyFailure() {
+//        this.student = new Student("300", "", 100, "ewofohewf");
+//        try {
+//            this.service.addStudent(this.student);
+//            assert false;
+//        }
+//        catch (Exception e) {}
+//    }
+//
+//    @Test
+//    void addStudentNumeNullFailure() {
+//        this.student = new Student("300", null, 100, "ewofohewf");
+//        try {
+//            this.service.addStudent(this.student);
+//            assert false;
+//        }
+//        catch (Exception e) {}
+//    }
+//
+//    @Test
+//    void addStudentGrupaLessThan0Failure() {
+//        this.student = new Student("300", "nfaib", -1, "ewofohewf");
+//        try {
+//            this.service.addStudent(this.student);
+//            assert false;
+//        }
+//        catch (Exception e) {}
+//    }
+//
+//    @Test
+//    void addStudentEmailEmptyFailure() {
+//        this.student = new Student("300", "nfaib", 100, "");
+//        try {
+//            this.service.addStudent(this.student);
+//            assert false;
+//        }
+//        catch (Exception e) {}
+//    }
+//
+//    @Test
+//    void addStudentEmailNullFailure() {
+//        this.student = new Student("300", "nfaib", 100, null);
+//        try {
+//            this.service.addStudent(this.student);
+//            assert false;
+//        }
+//        catch (Exception e) {}
+//    }
     @Test
     void addAssignmentSuccessful() {
         this.tema = new Tema("1", "work work work", 10, 10);
@@ -117,9 +120,40 @@ public class AppTest {
         }
         catch (Exception e) {}
     }
+
+    @Test
+    void addAssignmentAlreadyExists() {
+        this.tema = new Tema("1", "file repository", 2, 1);
+        try {
+            Tema tm = this.service.addTema(this.tema);
+            Assertions.assertEquals(tm, this.tema);
+        }
+        catch (Exception e) {}
+    }
+
     @Test
     void addAssignmentDescriptionEmptyFailure() {
         this.tema = new Tema("1", "", 10, 10);
+        try {
+            this.service.addTema(this.tema);
+            assert false;
+        }
+        catch (Exception e) {}
+    }
+
+    @Test
+    void addAssignmentDeadlineInvalidFailure() {
+        this.tema = new Tema("13", "desc", 0, 10);
+        try {
+            this.service.addTema(this.tema);
+            assert false;
+        }
+        catch (Exception e) {}
+    }
+
+    @Test
+    void addAssignmentPrimireInvalidFailure() {
+        this.tema = new Tema("13", "desc", 10, 0);
         try {
             this.service.addTema(this.tema);
             assert false;
